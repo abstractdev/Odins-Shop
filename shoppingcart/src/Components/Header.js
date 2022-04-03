65db943a9fbcb9952b1528d231c8ec1b71470a83
fadcny <fadcny@gmail.com>
2022-04-03 19:44:14 -0400

    Links and cart icon

--- shoppingcart/src/Components/Header.js
+++ shoppingcart/src/Components/Header.js
@@ -1,16 +1,22 @@
 import '../Styles/Header.css'
-import odinIcon from '../Images/odinIcon.svg'
+import odinIcon from '../Assets/odinIcon.svg'
+import {BsCart} from 'react-icons/bs'
 
 export default function Header() {
   return (
     <>
     <nav className="top-nav">
-      <div className="icon-plus-text-container">
+      <a className="icon-plus-text-container" href="/home">
         <div className="odin-icon-container">
           <img className='odin-icon' src={odinIcon} alt="svg"/>
         </div>
         <div className="icon-text">The Odin Shop</div>
-      </div>
+      </a>
+        <div className="links-container">
+          <a className="home-link" href="/home">Home</a>
+          <a className="shop-link" href="/shop">Shop</a>
+          <a className="cart-link" href="/cart"><BsCart className='cart-icon' size='23px'/></a>
+        </div>
     </nav>
     </>
   );
