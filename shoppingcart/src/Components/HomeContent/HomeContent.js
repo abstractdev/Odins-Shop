e188ff6e651d2f14e56eb401a3ea5e5659df4d27
fadcny <fadcny@gmail.com>
2022-04-03 19:44:44 -0400

    HomeContent component initial

---
+++ shoppingcart/src/Components/HomeContent/HomeContent.js
@@ -0,0 +1,12 @@
+import '../../Styles/HomeContent.css'
+
+function HomeContent() {
+  return (
+    <>
+    <div className="home-content">
+      {/* <div className="men-items"></div> */}
+    </div>
+    </>
+  );
+}
+export default HomeContent
