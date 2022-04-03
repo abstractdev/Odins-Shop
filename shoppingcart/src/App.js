5f42347fd2ef7cf1285de5193074a5fd75cc9d0a
fadcny <fadcny@gmail.com>
2022-04-03 19:43:39 -0400

    import product data

--- shoppingcart/src/App.js
+++ shoppingcart/src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
+import { menTees, womenTees } from './Data/Tee';
+import { menHoodies, womenHoodies } from './Data/Hoodie';
+import { accesories } from './Data/Accessory';
 import Header from './Components/Header';
-// import foundation from './Images/foundation.svg'
-// import javascript from './Images/javascript.svg'
-// import node from './Images/node.svg'
-// import ruby from './Images/ruby.svg'
-// import html from './Images/html.svg'
-/* <img className='foundation' src={foundation} alt="svg" />
-      <img className='javascript' src={javascript} alt="svg" />
-      <img className='node' src={node} alt="svg" />
-      <img className='ruby' src={ruby} alt="svg" />
-      <img className='html' src={html} alt="svg" /> */
+import HomeContent from './Components/HomeContent/HomeContent';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <HomeContent/>
     </div>
   );
 }
