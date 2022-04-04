7a747993a24dd17ab0e57b0da53117d119c5550b
fadcny <fadcny@gmail.com>
2022-04-03 20:15:35 -0400

    pass item data to Shop

--- shoppingcart/src/App.js
+++ shoppingcart/src/App.js
@@ -4,12 +4,14 @@ import { menHoodies, womenHoodies } from './Data/Hoodie';
 import { accesories } from './Data/Accessory';
 import Header from './Components/Header';
 import HomeContent from './Components/HomeContent/HomeContent';
+import Shop from './Components/Shop/Shop';
 
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <Header/>
+      <Shop menTees={menTees} womenTees={womenTees} menHoodies={menHoodies} womenHoodies={womenHoodies} accesories={accesories}/>
       <HomeContent/>
     </div>
   );
