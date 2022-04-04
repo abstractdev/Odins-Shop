5ec8267f90f0a485bfccf4ba2fea3b1edc280a07
fadcny <fadcny@gmail.com>
2022-04-04 08:26:03 -0400

    Test navbar links

--- shoppingcart/src/App.test.js
+++ shoppingcart/src/App.test.js
@@ -1,8 +1,24 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("App Component", () => {
+  it("renders Home Link", () => {
+    render(<App />);
+    expect(screen.getByRole("link", { name: "Home" })).toBeVisible();
+  });
+  it("renders Shop Link", () => {
+    render(<App />);
+    expect(screen.getByRole("link", { name: "Shop" })).toBeVisible();
+  });
+  it("renders Icon Text", () => {
+    render(<App />);
+    expect(screen.getByTestId("icon-text")).toBeVisible();
+  });
+  it("renders Cart Link", () => {
+    render(<App />);
+    expect(screen.getByTestId("cart-link")).toBeVisible();
+  });
 });
