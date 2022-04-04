96b83581236bd5b789b0ff6b9b2fa651352895fa
fadcny <fadcny@gmail.com>
2022-04-04 08:24:32 -0400

    Routing initial

---
+++ shoppingcart/src/RouteSwitch.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from "./App";
+import Shop from './Components/Shop';
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Shop" element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RouteSwitch;
--- shoppingcart/src/index.js
+++ shoppingcart/src/index.js
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App";
+import RouteSwitch from "./RouteSwitch";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <RouteSwitch />
   </StrictMode>
 );
