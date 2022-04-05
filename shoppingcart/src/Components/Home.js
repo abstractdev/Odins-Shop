eda1de032ac85bd5cc9b074e56339ff6bc6ce828
fadcny <fadcny@gmail.com>
2022-04-04 22:42:31 -0400

    enter button

--- shoppingcart/src/Components/Home.js
+++ shoppingcart/src/Components/Home.js
@@ -1,9 +1,11 @@
 import '../Styles/Home.css'
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <>
     <div className="Home">
+      <Link to="/shop"><button className="enter">Enter</button></Link>
     </div>
     </>
   );
