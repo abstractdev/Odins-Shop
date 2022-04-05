05e0e371ac2dbab6a7c8e35b5d432994a88134f5
abstractdev <fadcny@gmail.com>
2022-04-05 17:09:09 -0400

    added category and theme properties

--- shoppingcart/src/Data/Accessory.js
+++ shoppingcart/src/Data/Accessory.js
@@ -5,9 +5,11 @@ import odinPillowImage from'../Assets/odin-pillow.png'
 import odinStickersImage from'../Assets/odin-stickers.png'
 import odinToteImage from'../Assets/odin-t-w.jpg'
 
-const Accessory = (id, type, price, image, quantity) => {
+const Accessory = (id, category, theme, type, price, image, quantity) => {
   return {
     id,
+    category,
+    theme,
     type,
     price,
     image,
@@ -15,11 +17,11 @@ const Accessory = (id, type, price, image, quantity) => {
   }
  }
 
-export const odinCase1 = Accessory('acc0', 'case', 20, odinPhone1Image, 10)
-export const odinCase2 = Accessory('acc1','case', 20, odinPhone2Image, 10)
-export const odinMug = Accessory('acc2','mug', 11, odinMugImage, 10)
-export const odinPillow = Accessory('acc3','pillow', 12, odinPillowImage, 10)
-export const odinStickers = Accessory('acc4','sticker', 5, odinStickersImage, 10)
-export const odinTote = Accessory('acc5','tote', 9, odinToteImage, 10)
+export const odinCase1 = Accessory('acc0', 'accessory', 'odin', 'case', 20, odinPhone1Image, 10)
+export const odinCase2 = Accessory('acc1', 'accessory', 'odin','case', 20, odinPhone2Image, 10)
+export const odinMug = Accessory('acc2', 'accessory', 'odin','mug', 11, odinMugImage, 10)
+export const odinPillow = Accessory('acc3', 'accessory', 'odin', 'pillow', 12, odinPillowImage, 10)
+export const odinStickers = Accessory('acc4', 'accessory', 'odin', 'sticker', 5, odinStickersImage, 10)
+export const odinTote = Accessory('acc5', 'accessory', 'odin', 'tote', 9, odinToteImage, 10)
 
 export const accessories = [odinCase1, odinCase2, odinMug, odinPillow, odinStickers, odinTote]
