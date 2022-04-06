e5dc55fbd097ea6fa5ba6989ad038caee8bcce12
abstractdev <fadcny@gmail.com>
2022-04-05 20:16:50 -0400

    set state, prevent negative numbers, input onchange

--- shoppingcart/src/Components/QuantityCounter.js
+++ shoppingcart/src/Components/QuantityCounter.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
 import { FiPlusSquare, FiMinusSquare } from "react-icons/fi";
 import '../Styles/QuantityCounter.css'
 
 function QuantityCounter() {
+  const [quantity, setQuantity] = useState(1)
+
+function handleClickQuantity(event) {
+  if (event.target.className === 'minus-container') {
+    if (quantity === 1) {
+      return
+    } else {
+      setQuantity(quantity - 1)
+    }
+  }else if(event.target.className === 'plus-container'){
+    setQuantity(quantity + 1)
+  }
+}
+
   return (
     <>
       <div className="counter-container">
-        <div className="minus-container">
-          <FiMinusSquare className='minus'/>
+        <div className="minus-container"onClick={(event) => {handleClickQuantity(event)}}>
+            <FiMinusSquare className='minus'/>
         </div>
-        <input type="text" name="quantity" id="quantity" />
-        <div className="plus-container">
-          <FiPlusSquare className='plus'/>
+        <input type="number" name="quantity" id="quantity" min="1" value={quantity} onChange={(event) => setQuantity(parseInt(event.target.value))}/>
+        <div className="plus-container" onClick={(event) => {handleClickQuantity(event)}}>
+            <FiPlusSquare className='plus'/>
         </div>
       </div>
     </>
