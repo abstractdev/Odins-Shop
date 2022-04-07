2261ca59490721e3c2f9d94f48e5ed822e46681c
abstractdev <fadcny@gmail.com>
2022-04-07 11:20:58 -0400

    fixed size select logic

--- shoppingcart/src/Components/SizeSelect.js
+++ shoppingcart/src/Components/SizeSelect.js
@@ -1,21 +1,18 @@
 import React from 'react'
 import '../Styles/SizeSelect.css'
 
-function SizeSelect() {
+function SizeSelect({size, setSize, handleSetSize}) {
+  
   return (
     <>
-        <div className="size-container">
-          <div className="size-label-container">
-            <label htmlFor="size">Size</label>
-          </div>
-          <div className="size-select-container">
-            <select name="size" id="size">
-              <option value="small">S</option>
-              <option value="medium">M</option>
-              <option value="large">L</option>
-            </select>
-          </div>
-        </div>
+      <div className="size-select-container">
+        <select name="size" id="size" value={size} onChange={(event) => handleSetSize(event)}>
+          <option value="size">Size</option>
+          <option value="small">S</option>
+          <option value="medium">M</option>
+          <option value="large">L</option>
+        </select>
+      </div>
     </>
   );
 }
