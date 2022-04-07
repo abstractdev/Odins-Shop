265556724f5643e7f9f74685c07ce90628678421
abstractdev <fadcny@gmail.com>
2022-04-07 11:14:54 -0400

    addcart button initial

---
+++ shoppingcart/src/Components/AddCartButton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function AddCartButton() {
+  return (
+        <button className='add-cart-button' type="submit">Add To Cart</button>
+  );
+}
+export default AddCartButton
