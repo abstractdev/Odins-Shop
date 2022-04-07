72c10aa4e051464b87370a96f7b37fe471b5fa77
abstractdev <fadcny@gmail.com>
2022-04-07 11:20:14 -0400

    moved size and quantity state up to app

--- shoppingcart/src/App.js
+++ shoppingcart/src/App.js
@@ -13,10 +13,15 @@ import MainNavigation from './Components/MainNavigation';
 import Sidebar from './Components/Sidebar'
 import Footer from './Components/Footer';
 import Details from './Components/Details';
+import CartPage from './Components/CartPage';
 
 function App(props) {
   const [totalCost, setTotalCost] = useState(0);
-  const [items, setitems] = useState([...menTees, ...womenTees, ...menHoodies, ...womenHoodies, ...accessories])
+  const [items, setItems] = useState([...menTees, ...womenTees, ...menHoodies, ...womenHoodies, ...accessories])
+  const [cartItems, setCartItems] = useState([]);
+  const [size, setSize] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
 
   function createCategorizedPage(header, category) {
     return(
@@ -32,6 +37,21 @@ function App(props) {
     )
   }
 
+  function handleSetSize(event) {
+    setSize(event.target.value);
+  }
+
+  function handleSetQuantity(event) {
+    if (event.target.className === 'minus-container') {
+      if (quantity === 1) {
+        return
+      } else {
+        setQuantity(quantity - 1)
+      }
+    }else if(event.target.className === 'plus-container'){
+      setQuantity(quantity + 1)
+    }
+  }
   return (
     <>
       <BrowserRouter>
@@ -42,10 +62,10 @@ function App(props) {
           <Route path="/shop/men" element={<>{createCategorizedPage("Men", <Men menTees={menTees} menHoodies={menHoodies}/>)}</>}/>
           <Route path="/shop/women" element={<>{createCategorizedPage("Women", <Women womenTees={womenTees} womenHoodies={womenHoodies}/>)}</>}/>
           <Route path="/shop/accessories" element={<>{createCategorizedPage("Accessories", <Accessories accessories={accessories}/>)}</>}/>
-          <Route path="/shop/men/:id" element={<>{createCategorizedPage("", <Details items={items}/>)}</>}/>
-          <Route path="/shop/women/:id" element={<>{createCategorizedPage("", <Details items={items}/>)}</>}/>
-          <Route path="/shop/accessories/:id" element={<>{createCategorizedPage("", <Details items={items}/>)}</>}/>
-
+          <Route path="/shop/men/:id" element={<>{createCategorizedPage("", <Details items={items} size={size} setSize={setSize} quantity={quantity} setQuantity={setQuantity} cartItems={cartItems} setCartItems={setCartItems} handleSetSize={handleSetSize} handleSetQuantity={handleSetQuantity}/>)} </>}/>
+          <Route path="/shop/women/:id" element={<>{createCategorizedPage("", <Details items={items} size={size} setSize={setSize} quantity={quantity} setQuantity={setQuantity} cartItems={cartItems} setCartItems={setCartItems} handleSetSize={handleSetSize} handleSetQuantity={handleSetQuantity}/>)}</>}/>
+          <Route path="/shop/accessories/:id" element={<>{createCategorizedPage("", <Details items={items} size={size} setSize={setSize} quantity={quantity} setQuantity={setQuantity} cartItems={cartItems} setCartItems={setCartItems} handleSetSize={handleSetSize} handleSetQuantity={handleSetQuantity}/>)}</>}/>
+          <Route path="/cart" element={<><div className="main-container"><Sidebar/><CartPage totalCost={totalCost} setTotalCost={setTotalCost} cartItems={cartItems} setCartItems={setCartItems}/></div></>}/>
         </Routes>
         <Footer/>
       </BrowserRouter>
