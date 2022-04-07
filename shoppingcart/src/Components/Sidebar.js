df1a855df8d376c0ecd72e0a2cae3a83627183b0
abstractdev <fadcny@gmail.com>
2022-04-07 11:21:33 -0400

    made sidebar semi-collapsable

--- shoppingcart/src/Components/Sidebar.js
+++ shoppingcart/src/Components/Sidebar.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import '../Styles/Sidebar.css'
 import {FiMenu} from "react-icons/fi";
 
 function Sidebar(props) {
+  const [sidebarClicked, setSidebarClicked] = useState(false)
+  function toggleSidebar(event) {
+    event.preventDefault()
+    !sidebarClicked?
+    setSidebarClicked(true):
+    setSidebarClicked(false);
+  }
   return (
     <>
       <nav className="side-nav">
-        <a href="/"><FiMenu className="side-burger"/></a>
+        <div className="side-burger-container" onClick={(event) => toggleSidebar(event)}><a href="/"><FiMenu className="side-burger"/></a></div>
+        {!sidebarClicked ?
         <div className="side-links-container">
           <NavLink to="/shop/men"  className="men-link">Men</NavLink>
           <NavLink to="/shop/women"  className="women-link">Women</NavLink>
           <NavLink to="/shop/accessories"  className="accessories-link">Accessories</NavLink>
-        </div>
+        </div> :
+        <></>}
       </nav>
     </>
   );
