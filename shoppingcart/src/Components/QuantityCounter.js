e45c7f9cfa687eee76692020f71049e971a63e0e
abstractdev <fadcny@gmail.com>
2022-04-07 11:22:21 -0400

    moved quantity state to parent

--- shoppingcart/src/Components/QuantityCounter.js
+++ shoppingcart/src/Components/QuantityCounter.js
@@ -1,33 +1,23 @@
 import React from 'react'
-import { useState } from 'react';
 import { FiPlusSquare, FiMinusSquare } from "react-icons/fi";
 import '../Styles/QuantityCounter.css'
 
-function QuantityCounter() {
-  const [quantity, setQuantity] = useState(1)
-
-function handleClickQuantity(event) {
-  if (event.target.className === 'minus-container') {
-    if (quantity === 1) {
-      return
-    } else {
-      setQuantity(quantity - 1)
-    }
-  }else if(event.target.className === 'plus-container'){
-    setQuantity(quantity + 1)
-  }
-}
+function QuantityCounter({quantity, setQuantity, handleSetQuantity}) {
 
   return (
     <>
       <div className="counter-container">
-        <div className="minus-container"onClick={(event) => {handleClickQuantity(event)}}>
-            <FiMinusSquare className='minus'/>
-        </div>
-        <input type="number" name="quantity" id="quantity" min="1" value={quantity} onChange={(event) => setQuantity(parseInt(event.target.value))}/>
-        <div className="plus-container" onClick={(event) => {handleClickQuantity(event)}}>
-            <FiPlusSquare className='plus'/>
-        </div>
+          <div className="minus-input-plus-container">
+            <div className="minus-container"onClick={(event) => {handleSetQuantity(event)}}>
+                <FiMinusSquare className='minus'/>
+            </div>
+            <div className="input-container">
+              <input type="number" name="quantity" id="quantity" min="1" value={quantity} onChange={(event) => setQuantity(parseInt(event.target.value))}/>
+            </div>
+            <div className="plus-container" onClick={(event) => {handleSetQuantity(event)}}>
+                <FiPlusSquare className='plus'/>
+            </div>
+          </div>
       </div>
     </>
   );
