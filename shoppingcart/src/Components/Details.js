0e6690fb3e0fe77982ff0170404916817f542ac3
abstractdev <fadcny@gmail.com>
2022-04-07 11:14:16 -0400

    created detailsItem state

--- shoppingcart/src/Components/Details.js
+++ shoppingcart/src/Components/Details.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import DetailsForm from "./DetailsForm";
 import '../Styles/Details.css';
 
-function Details({allStates}) {
+function Details({items, size, setSize, quantity, setQuantity, cartItems, setCartItems, handleSetSize, handleSetQuantity}) {
   let {id} = useParams();
-  const filtered = allStates.filter((element) => {
+  //find the item by matching the useparams id and the iteam id
+  const selectedItem = items.filter((element) => {
     return element.id === id
   });
+  //set a state variable equal to the item
+  const [detailsItem, setDetailsItem] = useState(selectedItem[0])
+  //display the item
   return (
       <>
+        {console.log(cartItems)}
         <div className="details-content">
-          {filtered[0].category === 'accessory' ?
-          <h1 className="details-header">{`${filtered[0].theme} ${filtered[0].type}`}</h1> :
-          <h1 className="details-header">{`${filtered[0].gender}'s ${filtered[0].color} ${filtered[0].theme} ${filtered[0].type}`}</h1>}
+          {detailsItem.category === 'accessory' ?
+          <h1 className="details-header">{`${detailsItem.theme} ${detailsItem.type}`}</h1> :
+          <h1 className="details-header">{`${detailsItem.gender}'s ${detailsItem.color} ${detailsItem.theme} ${detailsItem.type}`}</h1>}
           <div className="details-image-container">
-            <img src={filtered[0].image} alt="a"/>
+            <img src={detailsItem.image} alt="item"/>
           </div>
           <div className="details-bottom-container">
-            <DetailsForm/>
+            <DetailsForm detailsItem={detailsItem} setDetailsItem={setDetailsItem} size={size} setSize={setSize} quantity={quantity} setQuantity={setQuantity} cartItems={cartItems} setCartItems={setCartItems} handleSetSize={handleSetSize} handleSetQuantity={handleSetQuantity}/>
           </div>
         </div>
       </>
