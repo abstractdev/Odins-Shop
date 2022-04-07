50e67fbe56b036fe648ecd23837152fc65b95aed
abstractdev <fadcny@gmail.com>
2022-04-07 11:23:33 -0400

    added addtocart button and onsubmit function

--- shoppingcart/src/Components/DetailsForm.js
+++ shoppingcart/src/Components/DetailsForm.js
@@ -1,18 +1,33 @@
 import React from "react";
 import SizeSelect from "./SizeSelect";
 import QuantityCounter from "./QuantityCounter";
+import AddCartButton from "./AddCartButton";
 import '../Styles/DetailsForm.css'
+import '../Styles/AddCartButton.css'
 
-function DetailsForm() {
+function DetailsForm({detailsItem, size, setSize, quantity, setQuantity, cartItems, setCartItems, handleSetSize, handleSetQuantity}) {
+  console.log(size);
+console.log(cartItems);
+  function handleOnSubmit(event) {
+    event.preventDefault();
+    if (event.target.elements[0].value ==='size') {
+      alert('error')
+    }
+    setCartItems([...cartItems, {cartSize: size, cartQuantity: quantity, ...detailsItem}])
+  }
+
   return (
-    <>
-      <form className="DetailsForm">
+      <form className="DetailsForm" onSubmit={(event)=> handleOnSubmit(event)}>
         <div className="details-bottom-container">
-          <SizeSelect/>
-          <QuantityCounter/>
+          <div className="size-quantity-container">
+            <SizeSelect size={size} setSize={setSize} handleSetSize={handleSetSize}/>
+            <QuantityCounter quantity={quantity} setQuantity={setQuantity} detailsItem={detailsItem} handleSetQuantity={handleSetQuantity}/>
+          </div>
+          <div className="add-cart-button-container">
+          <AddCartButton/>
+          </div>
         </div>
       </form>
-    </>
   );
 }
 export default DetailsForm
