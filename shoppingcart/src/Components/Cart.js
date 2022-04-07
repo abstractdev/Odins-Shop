8bc693a87a6289b8d0c44280038ce43d0f312198
abstractdev <fadcny@gmail.com>
2022-04-07 13:05:05 -0400

    added cart quantity display in navbar

--- shoppingcart/src/Components/Cart.js
+++ shoppingcart/src/Components/Cart.js
@@ -3,11 +3,25 @@ import { NavLink } from 'react-router-dom';
 import {BsCart} from 'react-icons/bs'
 import '../Styles/Cart.css'
 
-function Cart() {
+function Cart({cartItems}) {
+
+  function getCartQuantity() {
+    const cartItemsQuantity = cartItems.map((e) => e.cartQuantity)
+    const cartQuantity = cartItemsQuantity.reduce((acc, cv) => {
+      return acc + cv
+    }, 0);
+    return cartQuantity
+  }
+
 
   return (
     <>
-      <NavLink to="/cart" data-testid="cart-link" className="cart-link"><BsCart className='cart-icon' size='23px'/></NavLink>
+      <NavLink to="/cart" data-testid="cart-link" className="cart-link">
+        <div className="cart-icon-container">
+          <BsCart className='cart-icon' size='23px'/>
+          <div className="cart-number">{cartItems.length > 0 && getCartQuantity()}</div>
+        </div>
+      </NavLink>
     </>
   );
 }
