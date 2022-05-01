409ade8fd3d3f7eea2d4708567e51396d26958b8
abstractdev <fadcny@gmail.com>
2022-05-01 17:20:21 -0400

    socials and contact links

--- shoppingcart/src/Components/Footer.js
+++ shoppingcart/src/Components/Footer.js
@@ -1,21 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import '../Styles/Footer.css'
+import "../Styles/Footer.css";
 import { FiMail } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
+import { FiTwitter } from "react-icons/fi";
+import { FiFacebook } from "react-icons/fi";
+import { FiGithub } from "react-icons/fi";
+import { FiInfo } from "react-icons/fi";
 
 function Footer(props) {
   return (
     <>
       <footer className="footer">
+        <div className="socials-links">
+          <a href="http://www.github.com/abstractdev">
+            <FiInstagram />
+          </a>
+          <a href="http://www.github.com/abstractdev">
+            <FiTwitter />
+          </a>
+          <a href="http://www.github.com/abstractdev">
+            <FiFacebook />
+          </a>
+          <a href="http://www.github.com/abstractdev">
+            <FiGithub />
+          </a>
+        </div>
         <div className="footer-links-container">
-          <Link to="/contact"  className="contact-link">
-              <div className="contact-icon-container">
-                <FiMail className="contact-icon"/>
-              </div>
-              <div className="footer-text-container">
-                Contact Us
-              </div>
-          </Link>
+          <a href="http://www.github.com/abstractdev" className="aboutus-link">
+            <div className="aboutus-icon-container">
+              <FiInfo className="aboutus-icon" />
+            </div>
+            <div className="aboutus-text-container">About Us</div>
+          </a>
+          <a href="http://www.github.com/abstractdev" className="contact-link">
+            <div className="contact-icon-container">
+              <FiMail className="contact-icon" />
+            </div>
+            <div className="footer-text-container">Contact Us</div>
+          </a>
         </div>
       </footer>
     </>
