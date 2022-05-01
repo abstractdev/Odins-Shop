c92222adc2bfda275b4201b32feee5703af21bd9
abstractdev <fadcny@gmail.com>
2022-05-01 17:17:54 -0400

    details component-shows details for each item

--- shoppingcart/src/Components/Details.js
+++ shoppingcart/src/Components/Details.js
@@ -1,32 +1,57 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import DetailsForm from "./DetailsForm";
-import '../Styles/Details.css';
+import "../Styles/Details.css";
 
-function Details({items, size, setSize, quantity, setQuantity, cartItems, setCartItems, handleSetSize, handleSetQuantity}) {
-  let {id} = useParams();
+function Details({
+  items,
+  size,
+  setSize,
+  quantity,
+  setQuantity,
+  cartItems,
+  setCartItems,
+  handleSetSize,
+  handleSetQuantity,
+}) {
+  let { id } = useParams();
   //find the item by matching the useparams id and the iteam id
   const selectedItem = items.filter((element) => {
-    return element.id === id
+    return element.id === id;
   });
   //set a state variable equal to the item
-  const [detailsItem, setDetailsItem] = useState(selectedItem[0])
+  const [detailsItem, setDetailsItem] = useState(selectedItem[0]);
   //display the item
   return (
-      <>
-        {console.log(cartItems)}
-        <div className="details-content">
-          {detailsItem.category === 'accessory' ?
-          <h1 className="details-header">{`${detailsItem.theme} ${detailsItem.type}`}</h1> :
-          <h1 className="details-header">{`${detailsItem.gender}'s ${detailsItem.color} ${detailsItem.theme} ${detailsItem.type}`}</h1>}
-          <div className="details-image-container">
-            <img src={detailsItem.image} alt="item"/>
-          </div>
-          <div className="details-bottom-container">
-            <DetailsForm detailsItem={detailsItem} setDetailsItem={setDetailsItem} size={size} setSize={setSize} quantity={quantity} setQuantity={setQuantity} cartItems={cartItems} setCartItems={setCartItems} handleSetSize={handleSetSize} handleSetQuantity={handleSetQuantity}/>
-          </div>
+    <>
+      <div className="details-content">
+        {detailsItem.category === "accessory" ? (
+          <h1 className="details-header">{`${detailsItem.theme} ${detailsItem.type}`}</h1>
+        ) : (
+          <h1 className="details-header">{`${detailsItem.gender}'s ${detailsItem.color} ${detailsItem.theme} ${detailsItem.type}`}</h1>
+        )}
+        <div className="details-image-container">
+          <input type="checkbox" name="zoom" id="zoom" />
+          <label htmlFor="zoom">
+            <img className="details-image" src={detailsItem.image} alt="item" />
+          </label>
         </div>
-      </>
+        <div className="details-bottom-container">
+          <DetailsForm
+            detailsItem={detailsItem}
+            setDetailsItem={setDetailsItem}
+            size={size}
+            setSize={setSize}
+            quantity={quantity}
+            setQuantity={setQuantity}
+            cartItems={cartItems}
+            setCartItems={setCartItems}
+            handleSetSize={handleSetSize}
+            handleSetQuantity={handleSetQuantity}
+          />
+        </div>
+      </div>
+    </>
   );
 }
-export default Details
+export default Details;
