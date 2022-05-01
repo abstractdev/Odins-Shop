87e0418f8d45d0493c0b5f7e273345254f51eb5f
abstractdev <fadcny@gmail.com>
2022-05-01 17:22:53 -0400

    shows all items

--- shoppingcart/src/Components/Shop.js
+++ shoppingcart/src/Components/Shop.js
@@ -3,23 +3,23 @@ import Men from "./Men";
 import Women from "./Women";
 import Accessories from "./Accessories";
 import Sidebar from "./Sidebar";
-import '../Styles/Shop.css'
+import "../Styles/Shop.css";
 
-function Shop({menTees, womenTees, menHoodies, womenHoodies, accessories}) {
+function Shop({ menTees, womenTees, menHoodies, womenHoodies, accessories }) {
   return (
     <>
       <div className="shop-container">
-        <Sidebar/>
+        <Sidebar />
         <div className="shop-content-container">
-          <div className="shop-header">Odin's Store</div>
+          <div className="shop-header">Odin's Shop</div>
           <div className="shop-content">
-              <Men menTees={menTees} menHoodies={menHoodies}/>
-              <Women womenTees={womenTees} womenHoodies={womenHoodies}/>
-              <Accessories accessories={accessories}/>
+            <Men menTees={menTees} menHoodies={menHoodies} />
+            <Women womenTees={womenTees} womenHoodies={womenHoodies} />
+            <Accessories accessories={accessories} />
           </div>
         </div>
       </div>
     </>
   );
 }
-export default Shop
+export default Shop;
