121ef01be3ea56c76df413d1a3eb88b8ee030f5e
abstractdev <fadcny@gmail.com>
2022-05-01 17:23:48 -0400

    desktop sidebar with collapse button

--- shoppingcart/src/Components/Sidebar.js
+++ shoppingcart/src/Components/Sidebar.js
@@ -1,27 +1,38 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import '../Styles/Sidebar.css'
-import {FiMenu} from "react-icons/fi";
+import "../Styles/Sidebar.css";
+import { FiMenu } from "react-icons/fi";
 
 function Sidebar(props) {
-  const [sidebarClicked, setSidebarClicked] = useState(false)
+  const [sidebarClicked, setSidebarClicked] = useState(false);
   function toggleSidebar(event) {
-    event.preventDefault()
-    !sidebarClicked?
-    setSidebarClicked(true):
-    setSidebarClicked(false);
+    event.preventDefault();
+    !sidebarClicked ? setSidebarClicked(true) : setSidebarClicked(false);
   }
   return (
     <>
       <nav className="side-nav">
-        <div className="side-burger-container" onClick={(event) => toggleSidebar(event)}><a href="/"><FiMenu className="side-burger"/></a></div>
-        {!sidebarClicked ?
-        <div className="side-links-container">
-          <NavLink to="/shop/men"  className="men-link">Men</NavLink>
-          <NavLink to="/shop/women"  className="women-link">Women</NavLink>
-          <NavLink to="/shop/accessories"  className="accessories-link">Accessories</NavLink>
-        </div> :
-        <></>}
+        <div
+          className="side-burger-container"
+          onClick={(event) => toggleSidebar(event)}
+        >
+          <FiMenu className="side-burger" />
+        </div>
+        {!sidebarClicked ? (
+          <div className="side-links-container">
+            <NavLink to="/shop/men" className="side-men-link">
+              Men
+            </NavLink>
+            <NavLink to="/shop/women" className="side-women-link">
+              Women
+            </NavLink>
+            <NavLink to="/shop/accessories" className="accessories-link">
+              Accessories
+            </NavLink>
+          </div>
+        ) : (
+          <></>
+        )}
       </nav>
     </>
   );
