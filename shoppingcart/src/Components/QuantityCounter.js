8be72079ba5ba79e3304bb877a5b66803cae52da
abstractdev <fadcny@gmail.com>
2022-05-01 23:47:53 -0400

    formatting

--- shoppingcart/src/Components/QuantityCounter.js
+++ shoppingcart/src/Components/QuantityCounter.js
@@ -1,25 +1,42 @@
-import React from 'react'
+import React from "react";
 import { FiPlusSquare, FiMinusSquare } from "react-icons/fi";
-import '../Styles/QuantityCounter.css'
+import "../Styles/QuantityCounter.css";
 
-function QuantityCounter({quantity, setQuantity, handleSetQuantity}) {
-
+function QuantityCounter({ quantity, setQuantity, handleSetQuantity }) {
   return (
     <>
       <div className="counter-container">
-          <div className="minus-input-plus-container">
-            <div className="minus-container"onClick={(event) => {handleSetQuantity(event)}}>
-                <FiMinusSquare className='minus'/>
-            </div>
-            <div className="input-container">
-              <input type="number" name="quantity" id="quantity" min="1" value={quantity} onChange={(event) => setQuantity(parseInt(event.target.value))}/>
-            </div>
-            <div className="plus-container" onClick={(event) => {handleSetQuantity(event)}}>
-                <FiPlusSquare className='plus'/>
-            </div>
+        <div className="minus-input-plus-container">
+          <div
+            className="minus-container"
+            onClick={(event) => {
+              handleSetQuantity(event);
+            }}
+          >
+            <FiMinusSquare className="minus" />
           </div>
+          <div className="input-container">
+            <input
+              type="number"
+              name="quantity"
+              id="quantity"
+              min="1"
+              value={quantity}
+              onChange={(event) => setQuantity(parseInt(event.target.value))}
+            />
+          </div>
+          <div
+            data-testid="plus"
+            className="plus-container"
+            onClick={(event) => {
+              handleSetQuantity(event);
+            }}
+          >
+            <FiPlusSquare className="plus" />
+          </div>
+        </div>
       </div>
     </>
   );
 }
-export default QuantityCounter
+export default QuantityCounter;
